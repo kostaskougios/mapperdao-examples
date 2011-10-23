e63727cdaecdf4667c0178bff268ff6ed841fb3d
kostas.kougios <kostas.kougios@googlemail.com>
2011-10-23 21:59:39 +0000

    spec completed for product dao

--- productcatalogue/src/test/scala/com/rits/dao/ProductDaoSpec.scala
+++ productcatalogue/src/test/scala/com/rits/dao/ProductDaoSpec.scala
@@ -12,7 +12,48 @@ class ProductDaoSpec extends SpecificationWithJUnit {
 	val attributesDao = Daos.attributesDao
 	val categoriesDao = Daos.categoriesDao
 
-	"persist" in {
+	"update prices" in {
+		val p = Product("test1", "desc1", Set(Price("GBP", 10.5, 9.99), Price("EUR", 12.50, 11.05)), Set(), List(), Set())
+		val created = productsDao.create(p)
+
+		val newP = new Product(created.title, created.description, created.prices.filterNot(_.currency == "GBP"), created.attributes, created.categories, created.tags)
+		val updated = productsDao.update(created, newP)
+		updated must_== newP
+	}
+
+	"persist attributes" in {
+		val a1 = attributesDao.getOrCreate("performance", "fast")
+		val a2 = attributesDao.getOrCreate("security", "extra secure")
+		val p = productsDao.create(Product("test1", "desc1", Set(), Set(a1, a2), List(), Set()))
+		productsDao.retrieve(p.id).get.attributes must_== Set(a1, a2)
+	}
+
+	"update attributes" in {
+		val a1 = attributesDao.getOrCreate("performance", "fast")
+		val a2 = attributesDao.getOrCreate("security", "extra secure")
+		val p = productsDao.create(Product("test1", "desc1", Set(), Set(a1, a2), List(), Set()))
+		val u = productsDao.update(p, Product(p.title, p.description, Set(), Set(a1), List(), Set()))
+		productsDao.retrieve(u.id).get.attributes must_== Set(a1)
+	}
+
+	"persist categories" in {
+		val cat1 = categoriesDao.createHierarchy(List("Operating Systems", "iMac"))
+		val cat2 = categoriesDao.createHierarchy(List("Operating Systems", "iMac", "Leopard"))
+		val categories = List(cat1, cat2)
+		val p = productsDao.create(Product("test1", "desc1", Set(), Set(), categories, Set()))
+		productsDao.retrieve(p.id).get.categories.toSet must_== Set(cat1, cat2)
+	}
+
+	"update categories" in {
+		val cat1 = categoriesDao.createHierarchy(List("Operating Systems", "iMac"))
+		val cat2 = categoriesDao.createHierarchy(List("Operating Systems", "iMac", "Leopard"))
+		val categories = List(cat1, cat2)
+		val p = productsDao.create(Product("test1", "desc1", Set(), Set(), categories, Set()))
+		val u = productsDao.update(p, Product("test1", "desc1", Set(), Set(), List(cat1), Set()))
+		productsDao.retrieve(p.id).get.categories.toSet must_== Set(cat1)
+	}
+
+	"persist combined" in {
 		val a1 = attributesDao.getOrCreate("performance", "fast")
 		val a2 = attributesDao.getOrCreate("security", "extra secure")
 		val attributes = Set(a1, a2)
@@ -26,24 +67,19 @@ class ProductDaoSpec extends SpecificationWithJUnit {
 		retrieved must_== created
 	}
 
-	"update prices" in {
+	"update combined" in {
 		val a1 = attributesDao.getOrCreate("performance", "fast")
 		val a2 = attributesDao.getOrCreate("security", "extra secure")
 		val attributes = Set(a1, a2)
-		val cat = categoriesDao.createHierarchy(List("Operating Systems", "iMac"))
-		val categories = List(cat)
-		val p = Product("test1", "desc1", Set(Price("GBP", 10.5, 9.99), Price("EUR", 12.50, 11.05)), attributes, categories, Set())
-		val created = productsDao.create(p)
 
-		val newP = new Product(created.title, created.description, created.prices.filterNot(_.currency == "GBP"), created.attributes, created.categories, created.tags)
-		val updated = productsDao.update(created, newP)
-		updated must_== newP
-	}
-
-	"persist attributes" in {
-		val a1 = attributesDao.getOrCreate("performance", "fast")
-		val a2 = attributesDao.getOrCreate("security", "extra secure")
-		val p = productsDao.create(Product("test1", "desc1", Set(), Set(a1, a2), List(), Set()))
-		productsDao.retrieve(p.id).get must_== p
+		val cat1 = categoriesDao.createHierarchy(List("Operating Systems", "Linux"))
+		val cat2 = categoriesDao.createHierarchy(List("Operating Systems", "Leopard"))
+		val categories = List(cat1, cat2)
+		val p = productsDao.create(Product("test1", "desc1", Set(Price("GBP", 10.5, 9.99), Price("EUR", 12.50, 11.05)), attributes, categories, Set()))
+		val u = productsDao.update(p, Product("test1", "desc1", p.prices.filterNot(_.currency == "GBP"), Set(a1), List(cat2), Set()))
+		val r = productsDao.retrieve(u.id).get
+		r.prices must_== Set(Price("EUR", 12.50, 11.05))
+		r.attributes must_== Set(a1)
+		r.categories must_== List(cat2)
 	}
 }
