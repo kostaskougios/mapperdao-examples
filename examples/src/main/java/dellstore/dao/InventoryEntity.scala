afaae179e226fcff1c050188f316618206555925
kostas.kougios <kostas.kougios@googlemail.com>
2012-09-04 22:40:56 +0100

    extra scaladocs

--- examples/src/main/java/dellstore/dao/InventoryEntity.scala
+++ examples/src/main/java/dellstore/dao/InventoryEntity.scala
@@ -3,6 +3,17 @@ package dellstore.dao
 import com.googlecode.mapperdao._
 import dellstore.model._
 
+/**
+ * maps to this table:
+ *
+ * CREATE TABLE inventory
+ * (
+ * prod_id integer NOT NULL,
+ * quan_in_stock integer NOT NULL,
+ * sales integer NOT NULL,
+ * CONSTRAINT inventory_pkey PRIMARY KEY (prod_id )
+ * )
+ */
 object InventoryEntity extends SimpleEntity[Inventory]("inventory") {
 	val stock = column("quan_in_stock") to (_.stock)
 	val sales = column("sales") to (_.sales)
