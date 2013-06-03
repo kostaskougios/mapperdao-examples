da3eec9f9fc4ac751467f15491da4d8df2c624b7
kostas.kougios@gmail.com <sb3Xk9Eq5Nm2>
2013-06-03 21:56:06 +0100

    updated examples

--- productcatalogue/src/main/scala/com/rits/dao/Daos.scala
+++ productcatalogue/src/main/scala/com/rits/dao/Daos.scala
@@ -1,7 +1,7 @@
 package com.rits.dao
+
 import java.util.Properties
 import com.googlecode.mapperdao.utils.Setup
-import com.googlecode.mapperdao.jdbc.Transaction
 import org.apache.commons.dbcp.BasicDataSourceFactory
 import com.googlecode.mapperdao.utils.Database
 
@@ -11,9 +11,10 @@ import com.googlecode.mapperdao.utils.Database
  *
  * @author kostantinos.kougios
  *
- * 10 Oct 2011
+ *         10 Oct 2011
  */
-object Daos {
+object Daos
+{
 	// database connectivity setup (private to this factory)
 	// We'll use apache's basic data source to pool the connections
 	private val properties = new Properties
@@ -27,19 +28,22 @@ object Daos {
 	private val dataSource = BasicDataSourceFactory.createDataSource(properties)
 
 	// and we'll connect to the database, registering UserEntiry,SecretEntity...
-	private val entities = List(ProductEntity, CategoryEntity, AttributeEntity, PriceEntity)
+	private val entities = List(ProductEntity, ProductEntity.TagsEntity, CategoryEntity, AttributeEntity, PriceEntity)
 	private val (j, md, q, txM) = Setup(Database.byName(database), dataSource, entities, None)
 
 	// and now the dao singletons
-	val productsDao = new ProductsDao {
+	val productsDao = new ProductsDao
+	{
 		val (mapperDao, queryDao, txManager) = (md, q, txM)
 	}
 
-	val attributesDao = new AttributesDao {
+	val attributesDao = new AttributesDao
+	{
 		val (mapperDao, queryDao, txManager) = (md, q, txM)
 	}
 
-	val categoriesDao = new CategoriesDao {
+	val categoriesDao = new CategoriesDao
+	{
 		val (mapperDao, queryDao, txManager) = (md, q, txM)
 	}
 
